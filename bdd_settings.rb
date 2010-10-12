217670ddd7189a8e473a25c04728f378be40d1a5
Jesse Wolgamott <jesse.wolgamott@gmail.com>
2010-10-12 16:01:02 -0500

    Remove the login_module requirement from rspec helper

--- bdd_settings.rb
+++ bdd_settings.rb
@@ -13,9 +13,7 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'authlogic/test_case'
-require 'login_module'
 include Authlogic::TestCase
-include LoginModule
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
